fb384330e6546a289725574abe9842b8c8a9c420
Lucas Ferreira Nogueira <lucas.ferreiranogueira@outlook.com>
2025-02-03 14:50:14 -0300

    REFACTOR: mudanças para ambiente de produção espelhar o de desenvolvimento

--- index.php
+++ index.php
@@ -3,7 +3,6 @@ namespace General\EmailSending;
 
 use Dotenv\Dotenv;
 use General\EmailSending\Application\Controller\LeadController;
-use General\EmailSending\Application\Controller\ServiceController;
 use General\EmailSending\Application\Response\Response;
 
 require_once "./vendor/autoload.php";
@@ -12,7 +11,7 @@ $dotenv = Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: GET, POST");
+header("Access-Control-Allow-Methods: POST");
 
 $method = $_SERVER['REQUEST_METHOD'];
 $uri = $_SERVER['REQUEST_URI'];
@@ -28,19 +27,7 @@ switch ($uri) {
                 $leadController->create($data);
 
                 break;
-            case 'GET':
-
-                $leadController->getAll();
-
-                break;
         }
-        break;
-    case '/services':
-
-        $serviceController = new ServiceController();
-        $serviceController->getAll();
-
-        break;
     default:
 
         Response::notFound(404, 'Rota não encontrada');
